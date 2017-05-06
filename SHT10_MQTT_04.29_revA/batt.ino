35631f71cdd5ac7b6e0509125c5d325c652b0b3c
damal52 <davmal5@Gmail.com>
2017-05-06 12:14:55 -0500

    Add files via upload
    add function for reading battery voltage

---
+++ SHT10_MQTT_04.29_revA/batt.ino
@@ -0,0 +1,7 @@
+float battVoltage() {
+  // read the input on analog pin A0:
+//  int adcValue = analogRead(A3);
+//  float pinVoltage = adcValue * (3.25 / 4095.0);  // 12 bits between GND and Vcc
+//  float sourceVoltage = 1.996 * pinVoltage;
+//  return(sourceVoltage);
+}
